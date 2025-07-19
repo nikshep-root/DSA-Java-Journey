c057435e58e91fd0a688702b5d99eed699d2e3f9
Nikshep <nikshepd01@gmail.com>
2025-07-20 00:00:10 +0530

    checking even or odd program done

---
+++ 01-Basics/even_odd.java
@@ -0,0 +1,17 @@
+//package 01-Basics;
+import java.util.Scanner;
+
+public class even_odd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        if (number % 2 ==0){
+            System.out.println(number + " is even");
+        }
+        else{
+            System.out.println(number + " is odd");
+        }
+        }
+    }
+
