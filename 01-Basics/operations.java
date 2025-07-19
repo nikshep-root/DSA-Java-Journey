af66c0a3f8d3c1034266d2e72592f0f9ed419cd6
Nikshep <nikshepd01@gmail.com>
2025-07-20 00:38:03 +0530

    operations prg completed

---
+++ 01-Basics/operations.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+public class operations {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter first number: ");
+        float a = sc.nextFloat();
+        System.out.println("Enter secont number: ");
+        float b = sc.nextFloat();
+        System.out.println("Addition: " + (int)(a +b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+    }
+}
