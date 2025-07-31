6210dc3a9611029f6c0d520d55821a48c48f5cbb
Nikshep <nikshepd01@gmail.com>
2025-07-31 23:58:49 +0530

    Today's programs updated

---
+++ 02-Conditionals-loops/Basic-prgs/Basic-prgs/Average.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Average {
+      public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements:");
+        int n =1;
+        double sum = 0.0;
+        for(int i = 0 ; i < n; i++) {
+            System.out.println("Enter number " + (i + 1) + ":");
+            double num = sc.nextDouble();
+            sum += num;
+        }
+        double avg = sum / n;
+        System.out.println("The average is: " + avg);
+      }    
+} 
