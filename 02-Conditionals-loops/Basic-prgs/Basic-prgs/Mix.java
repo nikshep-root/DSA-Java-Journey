e78ff4553e2be603e45284f5f073ecc0c9da3251
Nikshep <nikshepd01@gmail.com>
2025-08-06 23:52:33 +0530

    Today's program updated

---
+++ 02-Conditionals-loops/Basic-prgs/Basic-prgs/Mix.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Mix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = 1;
+        int negCount = 0;
+        int posEvenCount = 0;
+        int posOddCount = 0;
+        List<Integer> arr = new ArrayList<>();
+        while (num != 0) {
+            System.out.println("Enter a number : ");
+            num = sc.nextInt();
+            if (num ==0) {
+                System.out.println("You entered 0, exiting the loop.");
+            }
+            arr.add(num);
+            if (num < 0) {
+                negCount += num;
+            }
+            else if (num > 0 && num % 2 == 0) {
+                posEvenCount += num;
+            } else if (num > 0 && num % 2 != 0) {
+                posOddCount += num;
+            }
+        }
+        System.out.println("You entered the following numbers: " + arr);
+        System.out.println("Sum of negative numbers: " + negCount);
+        System.out.println("Sum of positive even numbers: " + posEvenCount);
+        System.out.println("Sum of positive odd numbers: " + posOddCount);
+    }
+}
