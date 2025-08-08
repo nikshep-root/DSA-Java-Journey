43d88c80d458c5b3c3b0c50df1b07794a3cea2de
Nikshep <nikshepd01@gmail.com>
2025-08-08 23:58:27 +0530

    Today's prg updated

--- 02-Conditionals-loops.java/Basic-prgs/Basic-prgs/Factors.java
+++ 02-Conditionals-loops.java/Basic-prgs/Basic-prgs/Factors.java
@@ -1,14 +1,3 @@
-import java.util.Scanner;
 public class Factors {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        System.out.println("Enter a number to find its factors:");
-        int number = sc.nextInt();
-        System.out.println("Factors of " + number + " are:");
-        for (int i= 1; i<=number; i++) {
-            if (number % i == 0) {
-                System.out.println(i);
-            }
-        }
-    }
+    
 }
